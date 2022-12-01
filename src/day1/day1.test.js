2196731c3cc197a8442b134ed602683599c17300
Jonas Andersson Hultgren <jonas.andersson00@gmail.com>
2022-12-01 21:28:39 +0100

    test: add test cases for puzzle answers

--- src/day1/day1.test.js
+++ src/day1/day1.test.js
@@ -1,6 +1,7 @@
+const getInput = require('../get-input');
 const { doPart1, doPart2 } = require('./day1');
 
-const input = `1000
+const testInput = `1000
 2000
 3000
 
@@ -16,12 +17,16 @@ const input = `1000
 10000
 `;
 
+const realInput = getInput(1);
+
 describe('day1', () => {
   test('part 1', () => {
-    expect(doPart1(input)).toBe(24000);
+    expect(doPart1(testInput)).toBe(24000);
+    expect(doPart1(realInput)).toBe(69795);
   });
 
   test('part 2', () => {
-    expect(doPart2(input)).toBe(45000);
+    expect(doPart2(testInput)).toBe(45000);
+    expect(doPart2(realInput)).toBe(208437);
   });
 });
