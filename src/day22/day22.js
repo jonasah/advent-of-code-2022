624389bee6fdbe9a792997ebfa262284a4b9cdac
Jonas Andersson Hultgren <jonas.andersson00@gmail.com>
2022-12-29 18:05:45 +0100

    wip

--- src/day22/day22.js
+++ src/day22/day22.js
@@ -32,11 +32,19 @@ const parseInput = (input) => {
   return [parseMap(mapLines), parsePath(pathLine)];
 };
 
+const isWall = (map, pos) => {
+  return map[pos[1]][pos[0]] === WALL;
+};
+
 const findLeftMostPosition = (rowOrColumn) => {
   return rowOrColumn.findIndex((tile) => tile !== EMPTY);
 };
 
-const moveInRowOrColumn = (rowOrColumn, current, steps) => {
+const moveInRowOrColumn = (rowOrColumn, current, steps, edgePosition) => {
+  if (edgePosition == null) {
+    throw new Error('no edge');
+  }
+
   // find wall ahead
   const newPosition = current + steps;
   const wallPosition = rowOrColumn.indexOf(WALL, current);
@@ -46,56 +54,52 @@ const moveInRowOrColumn = (rowOrColumn, current, steps) => {
     return [wallPosition - 1, 0];
   }
 
-  // no wall, find edge ahead
-  let edgePosition = rowOrColumn.indexOf(EMPTY, current);
-
-  if (edgePosition === -1) {
-    edgePosition = rowOrColumn.length;
-  }
-
-  if (newPosition < edgePosition) {
-    // no edge, move steps without wrapping around
+  if (newPosition <= edgePosition) {
+    // will not reach edge, move steps without wrapping around
     return [newPosition, 0];
   }
 
-  const leftMostPosition = findLeftMostPosition(rowOrColumn);
-
-  if (rowOrColumn[leftMostPosition] === WALL) {
-    // wall on left edge, stop at right edge
-    return [edgePosition - 1, 0];
-  }
-
-  // wrap around to left edge
+  // move to edge
   const stepsLeft = newPosition - edgePosition;
-  return [leftMostPosition, stepsLeft];
+  return [edgePosition, stepsLeft];
 };
 
-const moveRight = (map, pos, steps) => {
+const moveRight = (map, pos, steps, edgePosition) => {
   const row = map[pos[1]];
-  const [newColumn, stepsLeft] = moveInRowOrColumn(row, pos[0], steps);
+  const [newColumn, stepsLeft] = moveInRowOrColumn(row, pos[0], steps, edgePosition);
   const newPos = [newColumn, pos[1]];
   return [newPos, stepsLeft];
 };
 
-const moveDown = (map, pos, steps) => {
+const moveDown = (map, pos, steps, edgePosition) => {
   const column = map.map((row) => row[pos[0]]);
-  const [newRow, stepsLeft] = moveInRowOrColumn(column, pos[1], steps);
+  const [newRow, stepsLeft] = moveInRowOrColumn(column, pos[1], steps, edgePosition);
   const newPos = [pos[0], newRow];
   return [newPos, stepsLeft];
 };
 
-const moveLeft = (map, pos, steps) => {
+const moveLeft = (map, pos, steps, edgePosition) => {
   const reversedRow = [...map[pos[1]]].reverse();
   const maxIndex = reversedRow.length - 1;
-  const [newColumn, stepsLeft] = moveInRowOrColumn(reversedRow, maxIndex - pos[0], steps);
+  const [newColumn, stepsLeft] = moveInRowOrColumn(
+    reversedRow,
+    maxIndex - pos[0],
+    steps,
+    maxIndex - edgePosition
+  );
   const newPos = [maxIndex - newColumn, pos[1]];
   return [newPos, stepsLeft];
 };
 
-const moveUp = (map, pos, steps) => {
+const moveUp = (map, pos, steps, edgePosition) => {
   const reversedColumn = map.map((row) => row[pos[0]]).reverse();
   const maxIndex = reversedColumn.length - 1;
-  const [newRow, stepsLeft] = moveInRowOrColumn(reversedColumn, maxIndex - pos[1], steps);
+  const [newRow, stepsLeft] = moveInRowOrColumn(
+    reversedColumn,
+    maxIndex - pos[1],
+    steps,
+    maxIndex - edgePosition
+  );
   const newPos = [pos[0], maxIndex - newRow];
   return [newPos, stepsLeft];
 };
@@ -103,13 +107,28 @@ const moveUp = (map, pos, steps) => {
 const rotateLeft = (dir) => (dir === RIGHT ? UP : dir - 1);
 const rotateRight = (dir) => (dir === UP ? RIGHT : dir + 1);
 
-const findPassword = (input) => {
+const getFaceForPosition = (faces, pos) => {
+  return faces.find(
+    ([, ul, dr]) =>
+      ul[0] <= pos[0] && pos[0] <= dr[0] && ul[1] <= pos[1] && pos[1] <= dr[1]
+  );
+};
+
+const getFace = (faces, face) => {
+  return faces.find(([n]) => n === face);
+};
+
+const findPassword = (input, faces) => {
   const [map, path] = parseInput(input);
 
   let pos = [findLeftMostPosition(map[0]), 0];
   let dir = RIGHT;
 
+  let n = 0;
+  const hist = [];
+
   while (path.length > 0) {
+    n += 1;
     const action = path.shift();
 
     if (action === 'L') {
@@ -120,35 +139,80 @@ const findPassword = (input) => {
       const steps = action;
       let stepsLeft = 0;
 
+      const face = getFaceForPosition(faces, pos);
+      if (!face) {
+        console.log('no face', pos);
+        throw new Error('no face :(');
+      }
+      const [, ul, dr] = face;
+
       if (dir === RIGHT) {
-        [pos, stepsLeft] = moveRight(map, pos, steps);
+        [pos, stepsLeft] = moveRight(map, pos, steps, dr[0]);
       } else if (dir === DOWN) {
-        [pos, stepsLeft] = moveDown(map, pos, steps);
+        [pos, stepsLeft] = moveDown(map, pos, steps, dr[1]);
       } else if (dir === LEFT) {
-        [pos, stepsLeft] = moveLeft(map, pos, steps);
+        [pos, stepsLeft] = moveLeft(map, pos, steps, ul[0]);
       } else {
-        [pos, stepsLeft] = moveUp(map, pos, steps);
+        [pos, stepsLeft] = moveUp(map, pos, steps, ul[1]);
       }
 
       if (stepsLeft > 0) {
-        // continue moving in the same direction after wrapping around
-        path.unshift(stepsLeft);
+        const [, , , links] = face;
+        const [nextFaceNo, nextFaceDir] = links[dir];
+        const nextFace = getFace(faces, nextFaceNo);
+
+        if (!nextFace) {
+          throw new Error(`failed to find next ${nextFaceNo}`);
+        }
+
+        const wrappedPos = ((d, p) => {
+          const [, nextUL, nextDR] = nextFace;
+
+          if (d === RIGHT) {
+            return [nextUL[0], p[1]];
+          }
+          if (d === DOWN) {
+            return [p[0], nextUL[1]];
+          }
+          if (d === LEFT) {
+            return [nextDR[0], p[1]];
+          }
+
+          return [p[0], nextDR[1]];
+        })(dir, pos);
+
+        if (!isWall(map, wrappedPos)) {
+          pos = wrappedPos;
+          path.unshift(stepsLeft - 1);
+        }
       }
+
+      hist.push(pos);
+    }
+
+    if (Number.isNaN(pos[0]) || Number.isNaN(pos[1])) {
+      throw new Error(`nan ${hist.slice(-2, -1)} ${pos} ${action} ${dir}`);
+    }
+
+    if (n === 4) {
+      // break;
     }
   }
+
+  // console.log(hist);
 
   return 1000 * (pos[1] + 1) + 4 * (pos[0] + 1) + dir;
 };
 
-const doPart1 = (input) => {
-  return findPassword(input);
-};
+const doPart1 = findPassword;
 
-const doPart2 = (input, faceSize) => {
-  return findPassword(input);
-};
+const doPart2 = findPassword;
 
 module.exports = {
   doPart1,
   doPart2,
+  RIGHT,
+  DOWN,
+  LEFT,
+  UP,
 };
--- src/day22/day22.test.js
+++ src/day22/day22.test.js
@@ -1,5 +1,5 @@
 const getInput = require('../get-input');
-const { doPart1, doPart2 } = require('./day22');
+const { doPart1, doPart2, RIGHT, DOWN, LEFT, UP } = require('./day22');
 
 const testInput = `
         ...#
@@ -21,12 +21,128 @@ const realInput = getInput(22);
 
 describe('day22', () => {
   test('part 1', () => {
-    expect(doPart1(testInput)).toBe(6032);
-    expect(doPart1(realInput)).toBe(66292);
+    expect(
+      doPart1(testInput, [
+        [
+          1,
+          [8, 0],
+          [11, 3],
+          {
+            [RIGHT]: [1, LEFT],
+            [DOWN]: [4, UP],
+            [LEFT]: [1, RIGHT],
+            [UP]: [5, DOWN],
+          },
+        ],
+        [
+          2,
+          [0, 4],
+          [3, 7],
+          {
+            [RIGHT]: [3, LEFT],
+            [DOWN]: [2, UP],
+            [LEFT]: [4, RIGHT],
+            [UP]: [2, DOWN],
+          },
+        ],
+        [
+          3,
+          [4, 4],
+          [7, 7],
+          {
+            [RIGHT]: [4, LEFT],
+            [DOWN]: [3, UP],
+            [LEFT]: [2, RIGHT],
+            [UP]: [3, DOWN],
+          },
+        ],
+        [
+          4,
+          [8, 4],
+          [11, 7],
+          {
+            [RIGHT]: [2, LEFT],
+            [DOWN]: [5, UP],
+            [LEFT]: [3, RIGHT],
+            [UP]: [1, DOWN],
+          },
+        ],
+      ])
+    ).toBe(6032);
+    expect(
+      doPart1(realInput, [
+        [
+          1,
+          [50, 0],
+          [99, 49],
+          {
+            [RIGHT]: [2, LEFT],
+            [DOWN]: [3, UP],
+            [LEFT]: [2, RIGHT],
+            [UP]: [5, DOWN],
+          },
+        ],
+        [
+          2,
+          [100, 0],
+          [149, 49],
+          {
+            [RIGHT]: [1, LEFT],
+            [DOWN]: [2, UP],
+            [LEFT]: [1, RIGHT],
+            [UP]: [2, DOWN],
+          },
+        ],
+        [
+          3,
+          [50, 50],
+          [99, 99],
+          {
+            [RIGHT]: [3, LEFT],
+            [DOWN]: [5, UP],
+            [LEFT]: [3, RIGHT],
+            [UP]: [1, DOWN],
+          },
+        ],
+        [
+          4,
+          [0, 100],
+          [49, 149],
+          {
+            [RIGHT]: [5, LEFT],
+            [DOWN]: [6, UP],
+            [LEFT]: [5, RIGHT],
+            [UP]: [6, DOWN],
+          },
+        ],
+        [
+          5,
+          [50, 100],
+          [99, 149],
+          {
+            [RIGHT]: [4, LEFT],
+            [DOWN]: [1, UP],
+            [LEFT]: [4, RIGHT],
+            [UP]: [3, DOWN],
+          },
+        ],
+        [
+          6,
+          [0, 150],
+          [49, 199],
+          {
+            [RIGHT]: [6, LEFT],
+            [DOWN]: [4, UP],
+            [LEFT]: [6, RIGHT],
+            [UP]: [4, DOWN],
+          },
+        ],
+      ])
+    ).toBe(66292);
   });
 
   test('part 2', () => {
-    expect(doPart2(testInput, 4)).toBe(5031);
+    // expect(doPart2(testInput, 4)).toBe(5031);
     // expect(doPart2(realInput, 50)).toBe(false);
   });
 });
